d99d5112f71559b87e1bddfc53cffad9dcb8e5b8
mk812y <mk812y@gmail.com>
2023-01-21 21:30:35 +0300

    пока так

---
+++ DataPlaner/EventDetail.swift
@@ -0,0 +1,20 @@
+//
+//  EventDetail.swift
+//  DataPlaner
+//
+//  Created by Михаил Куприянов on 21.01.2023.
+//
+
+import SwiftUI
+
+struct EventDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        EventDetail()
+    }
+}
