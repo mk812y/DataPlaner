1617168b3ea5c31ab693ad3a30894292b54e9dc3
mk812y <mk812y@gmail.com>
2023-01-21 17:49:06 +0300

    Initial Commit

---
+++ DataPlaner/DataPlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataPlanerApp.swift
+//  DataPlaner
+//
+//  Created by Михаил Куприянов on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DataPlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
