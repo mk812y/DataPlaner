b67ee89fe7f1c77c2cfb89f1d9dc9689d7b56bcc
mk812y <mk812y@gmail.com>
2023-01-22 14:05:04 +0300

    done

--- DataPlaner/DataPlanerApp.swift
+++ DataPlaner/DataPlanerApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct DataPlanerApp: App {
+    @StateObject private var eventData = EventData()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                EventList()
+                Text("Select an Event")
+                    .foregroundColor(.secondary)
+            }
+            .environmentObject(eventData)
         }
     }
 }
